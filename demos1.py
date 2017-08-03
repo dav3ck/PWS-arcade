276a2415926a1cb1240ceff80223a9b305546392
dav3ck <davidkoymans@hotmail.com>
2017-08-03 16:08:34 +0200

    Add files via upload

---
+++ demos1.py
@@ -0,0 +1,19 @@
+x = ["HEY", "HOW", "ARE", "YOU", "fucking", "dumb", "fucks", "deze", "lijst", "moet", "langer"]
+
+#x = "0123456789"
+ 
+print("x=",x)
+ 
+# Benader een enkel character
+print("x[0]=",x[0])
+print("x[1]=",x[1])
+ 
+# Benaderen van de rechterkant
+print("x[-1]=",x[-1])
+ 
+# Benader 0-5
+print("x[:6]=",x[:6])
+# Benader 6
+print("x[6:]=",x[6:])
+# Benader 6-8
+print("x[6:9]=",x[6:9])
