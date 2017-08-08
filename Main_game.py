be781a52c62937af2bf2da1343886ce667b31c0a
Glennvuijk <Spyglenn@gmail.com>
2017-08-08 16:52:17 +0200

    Update Main_game.py

--- Main_game.py
+++ Main_game.py
@@ -1,7 +1,9 @@
 #libraries management
 import pygame
 pygame.init()
-import classes
+#from classes import *
+import fun
+import classes 
 
 #predetermined variables
 black = (0, 0, 0)
@@ -13,8 +15,10 @@ blue = (0,0,255)
 sc_w = 1000 #screen width
 sc_h = 800 #screen hight
 
-player = classes.moving_object()
-ball = classes.ball(50)
+player = classes.Moving_object()
+#ball = classes.Ball()
+
+balls = [] #empty list that'll hold all ze balls
 
 #setsup window
 screen = pygame.display.set_mode((sc_w,sc_h))
@@ -32,6 +36,8 @@ while True:
                 player.xspeed = -5
             elif event.key == pygame.K_RIGHT:
                 player.xspeed = 5
+            elif event.key == pygame.K_b:
+                balls.append(classes.Ball())
         elif event.type == pygame.KEYUP:
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 player.xspeed = 0
@@ -42,22 +48,25 @@ while True:
     player.xcord += player.xspeed #basic player movement
     player.ycord += player.yspeed
 
-    ball.yspeed -= ball.weight #handles ball bouncing
-    ball.ycord -= ball.yspeed
-    if ball.ycord >= (sc_h - ball.dia):
-        ball.yspeed = 10
+    for i in balls:
+        i.yspeed -= i.weight #handles ball bouncing
+        i.ycord -= i.yspeed
+        if i.ycord >= (sc_h - i.dia):
+            i.yspeed = 10
 
-    ball.xcord += ball.xspeed #handles ball horizontal movement
-    if ball.xcord >= (sc_w - ball.dia) or ball.xcord <= (0 + ball.dia):
-        ball.xspeed *= -1
+        i.xcord += i.xspeed #handles ball horizontal movement
+        if i.xcord >= (sc_w - i.dia) or i.xcord <= (0 + i.dia):
+            i.xspeed *= -1
     
     #Screen management
     screen.fill(black)
-
+    
     pygame.draw.rect(screen, green, [sc_w/2-25+player.xcord,sc_h-50+player.ycord,50,50] , 2)
-    pygame.draw.circle(screen, red, [ball.xcord, int(ball.ycord)], ball.dia, 5)
+    for i in balls:
+        pygame.draw.circle(screen, red, [i.xcord, int(i.ycord)], i.dia, 5)
 
     pygame.display.flip()
     
     #sets max fps
     clock.tick(60)
+
