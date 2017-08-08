0284776a57042ec43229e9152f280c3c3d4941b5
Glennvuijk <Spyglenn@gmail.com>
2017-08-08 16:52:54 +0200

    Update classes.py

--- classes.py
+++ classes.py
@@ -1,29 +1,17 @@
 #general moving class
-class moving_object():
+class Moving_object():
     def __init__(self):
         self.xspeed = 0 #horizontal speed
         self.yspeed = 0 #vertical speed
         self.xcord = 0 #x coördinate
         self.ycord = 0 #y coördinate
-
-    '''def left(self):
-        self.xcord -= self.hspeed
-
-    def right(self):
-        self.xcord += self.hspeed
-
-    def up(self):
-        self.ycord -= self.vspeed
-
-    def down(self):
-        self.ycord == self.vspeed'''
     
 #ball variable
-class ball():
-    def __init__(self, dia):
-        self.xcord = 100 #x coord
-        self.ycord = 70 #y coord
-        self.xspeed = 3 #horizontal speed
-        self.yspeed = 0 #vertical speed
+class Ball(Moving_object):
+    def __init__(self):
+        super().__init__()
+        self.xcord = 500
+        self.xspeed = 3
+        self.ycord = 70
         self.weight = 0.1 #size of the parabole bigger number smaller parabole
-        self.dia = dia
+        self.dia = 50 #diameter of the ball
