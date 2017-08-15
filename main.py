dd392137f91ff2e5f7a36f0d18c64032b4bfbb24
dav3ck <davidkoymans@hotmail.com>
2017-08-15 13:25:17 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -8,6 +8,13 @@ white = (255, 255, 255)
 green = (0, 255, 0)
 red = (255, 0, 0)
 blue = (0,0,255)
+withd = 1000 #Brete van scherm
+height = 800 #Hoogte van scherm
+Score = 0
+
+editor = False
+xlines = 50
+ylines = 50
 
 screen = pygame.display.set_mode((1000,800)) #setsup window
 pygame.display.set_caption('very early pre-alfa')
@@ -30,10 +37,17 @@ while True:
                 ball = Ball(1,500,70)
             elif event.key == pygame.K_SPACE:
                 bullet = Bullet(player.xcord,player.ycord)
+            elif event.key == pygame.K_e: #Enables Editor mode
+                editor = True
         elif event.type == pygame.KEYUP: #handles all key releases
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                 player.xspeed = 0
             
+    #Score tekst
+
+    pygame.font.init()
+    myfont = pygame.font.SysFont('Comic Sans MS', 30)
+    textsurface = myfont.render('Score: ' + str(Score), False, white)
 
     #Game logica
 
@@ -57,12 +71,28 @@ while True:
                 ball = Ball(4,ball.xcord,ball.ycord)
                 ball = Ball(5,ball.xcord,ball.ycord)
             else:
-                print("here needs to be score code or something")
-                
+                Score += 1
+
     #Screen management
     screen.fill(black)
     
     everything.draw(screen)
+
+    #Editor mode
+    if editor == True:
+        while xlines < withd or ylines < height:
+            pygame.draw.line(screen, white, (xlines, 0), (ylines, height))
+            pygame.draw.line(screen, white, (0, ylines), (withd, ylines))
+            xlines += 50
+            ylines += 50
+        xlines = 50
+        ylines = 50
+
+    #Display tekst    
+
+    screen.blit(textsurface,(0,0)) 
+
+    #Flip
     
     pygame.display.flip()
     
