9d7760ba78160bc907b4a6a1b0f26498e9dda47a
Glennvuijk <Spyglenn@gmail.com>
2017-08-22 17:12:24 +0200

    rounded up keyboard code for easy transfer

--- Keyboard select.py
+++ Keyboard select.py
@@ -11,20 +11,15 @@ screen_rect=screen.get_rect()
 pygame.display.set_caption("Keyboard")
 clock = pygame.time.Clock()
 
-capital = False
-name = "" #List with your name in it
-
-num = 1 #Variable die aangeeft welke letter we nu op zitten
 #Array met alle toetsen erin
-alphabet = [" ", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y" , "z", " ", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 white = (255, 255, 255)
 black = (0, 0, 0)
 
 everything = pygame.sprite.Group() #Your everything group
-keyboards = pygame.sprite.Group() #Keyboard sprite group, IDK If necessary
+
 keyboardanimation = [] #Array met alle Keyboard sprites erin
 
-for i in range(81): #Zelfde als voor slime animatie sprites alleen dit keer kleiner
+for i in range(81): #Zelfde als voor slime animatie sprites alleen dit keer kleiner 
     legacy0 = "Sprites/Keyboard/Itteration" + str(i) + ".png"
     keyboardanimation.append(legacy0)
 
@@ -40,12 +35,25 @@ class Keyboard(parent): #Keyboard class
         super().__init__()
         self.xcord = 200
         self.ycord = 200
+        self.num = 1
         self.image = pygame.image.load(keyboardanimation[1])
         self.rect = self.image.get_rect()
-        keyboards.add(self)
+        self.capital = False
+        self.name = ""
+        self.alphabet = (" ", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y" , "z", " ", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z")
+
+    def update(self):
+        if self.num > 40: #Zorgt ervoor dat het tussen de 1 tm 40 blijft
+            self.num = self.num - 40 
+        elif self.num < 1:
+            self.num = 40 + self.num
+        if self.capital == False: 
+            keyboard.image = pygame.image.load(keyboardanimation[self.num])
+        elif self.capital == True:
+            keyboard.image = pygame.image.load(keyboardanimation[self.num + 37])
 
 
-keyboard = Keyboard() #Keyboard list IDK if necessary
+keyboard = Keyboard() #Initiates keyboard
 
 
 
@@ -56,48 +64,37 @@ while True:
 
         elif event.type == pygame.KEYDOWN: #handles all keypresses
             if event.key == pygame.K_LEFT: 
-                num -= 1
+                keyboard.num -= 1
             elif event.key == pygame.K_RIGHT:
-                num += 1
+                keyboard.num += 1
             elif event.key == pygame.K_UP:
-                num -= 10
+                keyboard.num -= 10
             elif event.key == pygame.K_DOWN:
-                num += 10
+                keyboard.num += 10
             if event.key == pygame.K_SPACE:
-                if (capital == False and num < 38): 
-                    name = name + alphabet[num] #adds letter to list with name 
-                elif capital == True and num < 38:
-                    name = name + alphabet[num + 37] #adds Capital letter to name list
+                if (keyboard.capital == False and keyboard.num < 38): 
+                    keyboard.name = keyboard.name + keyboard.alphabet[keyboard.num] #adds letter to list with name 
+                elif keyboard.capital == True and keyboard.num < 38:
+                    keyboard.name = keyboard.name + keyboard.alphabet[keyboard.num + 37] #adds Capital letter to name list
                     capital = False #Zet capital terug naar false > je print no longer Capitals
-                elif num == 38:
-                    if capital == False: #Zet capital naar true, tenzij het al true is, dan zet het het terug naar False
-                        capital = True
+                elif keyboard.num == 38:
+                    if keyboard.capital == False: #Zet capital naar true, tenzij het al true is, dan zet het het terug naar False
+                        keyboard.capital = True
                     else:
-                        capital = False
-                elif num == 39: #Haalt een letter weg
-                    name = name[:-1]
-                elif num == 40: #Print je naam/ Dit is de plek waar je de name uitput
-                    print(name)
+                        keyboard.capital = False
+                elif keyboard.num == 39: #Haalt een letter weg
+                    keyboard.name = keyboard.name[:-1]
+                elif keyboard.num == 40: #Print je naam/ Dit is de plek waar je de name uitput
+                    print(keyboard.name)
 
                     
-    # DIT KAN DENK IK OOK IN EEN KEYBOARD/ UPDATE CLASS
-              
-    if num > 40: #Zorgt ervoor dat het tussen de 1 tm 40 blijft
-        num = num - 40 
-    elif num < 1:
-        num = 40 + num
-        
-    #Handeld alle sprites
-    
-    if capital == False: 
-        keyboard.image = pygame.image.load(keyboardanimation[num])
-    elif capital == True:
-        keyboard.image = pygame.image.load(keyboardanimation[num + 37])
-    keyboard.rect = keyboard.image.get_rect()
+
+
+    everything.update()
         
     #print de text bottom right
-    numtext = myfont.render(str(num), False, white)
-    nametext = myfont.render(name, False, white)
+    numtext = myfont.render(str(keyboard.num), False, white) #this is just a check
+    nametext = myfont.render(keyboard.name, False, white) #prints the name
 
     #screen management
     screen.fill(black)
